6ab2c6fed72cb4f9140fb69bda99c309e9ebfb64
mounika <venkymounika9676@gmail.com>
2026-07-08 21:11:08 +0530

    Added code review API

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from analyzer import review_code
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "AI Code Review Assistant Backend Running!"
+
+
+@app.route("/review", methods=["POST"])
+def review():
+
+    data = request.json
+
+    code = data["code"]
+
+    result = review_code(code)
+
+    return jsonify({
+        "review": result
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
